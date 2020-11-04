ea1b915d699125d4dea83021f244e770e3a6fd29
Yuri Arapov <yuridichesky@gmail.com>
2020-11-04 18:26:12 +0300

    RNA transcription.

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,4 +1,16 @@
+// RNA transcription exercise of Exercism.io Go track.
 package strand
 
+// Convern DNA strand to RNA.
 func ToRNA(dna string) string {
+	// `G` -> `C`
+	// `C` -> `G`
+	// `T` -> `A`
+	// `A` -> `U`
+	dna2rna := map[rune]rune{'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
+	rna := make([]rune, len(dna))
+	for i, n := range dna {
+		rna[i] = dna2rna[n]
+	}
+	return string(rna)
 }
