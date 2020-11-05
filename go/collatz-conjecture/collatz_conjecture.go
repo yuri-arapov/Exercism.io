a6c95785537ee4066db038218da671a3dc998328
Yuri Arapov <yuridichesky@gmail.com>
2020-11-05 23:46:20 +0300

    Collatz Conjecture solved.

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,1 +1,24 @@
+// Collatz Conjecture problem of Exercism.io Go Track.
 package collatzconjecture
+
+import "fmt"
+
+// Return number of steps needed to converge Collatz Conjecture from n to 1.
+// Error is not nil if n is not positive.
+func CollatzConjecture(n int) (int, error) {
+	//fmt.Printf("%d", n)
+	if n <= 0 {
+		//fmt.Printf("<-invalid argument\n")
+		return 0, fmt.Errorf("invalid argument %d", n)
+	}
+	s := 0
+	for ; n != 1; s++ {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+	}
+	//fmt.Printf("->%d\n", s)
+	return s, nil
+}
