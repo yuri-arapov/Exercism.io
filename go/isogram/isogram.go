060568e06eee6a9aa934de433bc5be02b8e52f3a
Yuri Arapov <yuridichesky@gmail.com>
2020-11-16 00:08:48 +0300

    Solve Isogram.

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,1 +1,17 @@
+// Isogram task of Exercism.io Go track.
 package isogram
+
+import "unicode"
+
+// Retrun true if word is isogram (does not contain duplicating letters).
+func IsIsogram(word string) bool {
+	count := make(map[rune]bool)
+	for _, r := range word {
+		r = unicode.ToUpper(r)
+		if unicode.IsLetter(r) && count[r] {
+			return false
+		}
+		count[r] = true
+	}
+	return true
+}
