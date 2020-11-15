62e5595e5a2b5a0a650a881564a505a769e4867a
Yuri Arapov <yuridichesky@gmail.com>
2020-11-16 01:06:31 +0300

    Solve reverse string.

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -1,1 +1,35 @@
+// Reverse-string task of Exercism.io Go track.
 package reverse
+
+// Reverse string. Simple and slow.
+func Reverse1(s string) string {
+	res := ""
+	for _, r := range s {
+		res = string(r) + res
+	}
+	return res
+}
+
+// Reverse string with some optimization in mind.
+func Reverse(s string) string {
+	runes := make([]rune, len(s))
+	count := 0
+
+	// Fill array of runes.
+	for _, r := range s {
+		runes[count] = r
+		count++
+	}
+
+	// 'count' now holds number of runes in the string.
+	// 'count' <= 'len(s)' as single rune may take more than one byte.
+
+	// Reverse array of runes.
+	h, t := 0, count-1
+	for h < t {
+		runes[h], runes[t] = runes[t], runes[h]
+		h++
+		t--
+	}
+	return string(runes[:count])
+}
