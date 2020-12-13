8347d119b1a1a0809bc2eff7556cf67eb87e0daf
YuraA <yuridichesky@gmail.com>
2020-12-13 19:19:50 +0300

    12 days: footprint improvements.

--- go/twelve-days/twelve_days.go
+++ go/twelve-days/twelve_days.go
@@ -1,37 +1,54 @@
 package twelve
 
-var song = []string{
-	"On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
-	"On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
-	"On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-	"On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-	"On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-	"On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-	"On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-	"On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-	"On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-	"On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-	"On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-	"On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."}
+var (
+	number = []string{
+		"first",
+		"second",
+		"third",
+		"fourth",
+		"fifth",
+		"sixth",
+		"seventh",
+		"eighth",
+		"ninth",
+		"tenth",
+		"eleventh",
+		"twelfth"}
+
+	gift = []string{
+		"a Partridge in a Pear Tree.",
+		"two Turtle Doves, ",
+		"three French Hens, ",
+		"four Calling Birds, ",
+		"five Gold Rings, ",
+		"six Geese-a-Laying, ",
+		"seven Swans-a-Swimming, ",
+		"eight Maids-a-Milking, ",
+		"nine Ladies Dancing, ",
+		"ten Lords-a-Leaping, ",
+		"eleven Pipers Piping, ",
+		"twelve Drummers Drumming, "}
+)
 
 // Song returns a song.
 func Song() string {
-	s := ""
+	song := ""
 	sep := ""
-	for _, line := range song {
-		s = s + sep + line
+	for i, _ := range number {
+		song = song + sep + Verse(i+1)
 		sep = "\n"
 	}
-	return s
+	return song
 }
 
-func Verse(line int) string {
-	line--
-	if line < 0 {
-		return ""
+// Verse returns i-th line of the song.
+func Verse(i int) string {
+	line := "On the " + number[i-1] + " day of Christmas my true love gave to me: "
+	for g := i - 1; g > 0; g-- {
+		line = line + gift[g]
 	}
-	if line >= len(song) {
-		return ""
+	if i > 1 {
+		line = line + "and "
 	}
-	return song[line]
+	return line + gift[0]
 }
